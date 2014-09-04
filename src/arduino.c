45d0fbf8d0fc9ccf5d2b814405022a9e815fbf4a
chris <christoph@roboterclub-freiburg.de>
2014-09-04 21:53:25 +0200

    digitalRead added digitalWrite extented

--- src/arduino.c
+++ src/arduino.c
@@ -3,6 +3,19 @@
 // led location on LPC810 mini-kit
 // http://www.lpcware.com/lpc800-mini-kit
 #define LED_LOCATION    (2)
+/* Pin setup generated via Switch Matrix Tool
+   ------------------------------------------------
+   Package	Logic	 Function
+   =======  =====    ========
+   PIN1		PIO0_5 = RESET 			// not usable
+   PIN2		PIO0_4 = U0_TXD
+   PIN3		PIO0_3 = GPIO
+   PIN4		PIO0_2 = GPIO 			// User LED
+   PIN5		PIO0_1 = GPIO			// ISP Switch
+   PIN8		PIO0_0 = U0_RXD
+   ------------------------------------------------
+   NOTE: SWD is disabled to free GPIO pins!
+   ------------------------------------------------ */
 
 void pinMode(uint8_t pin, uint8_t mode)
 {
@@ -10,6 +23,11 @@ void pinMode(uint8_t pin, uint8_t mode)
 	if(pin==13)
 	{
 	    if(mode==OUTPUT) LPC_GPIO_PORT->DIR0 |= (1 << LED_LOCATION);
+	    else LPC_GPIO_PORT->DIR0 &= ~(1 << LED_LOCATION);
+	}else
+	{
+		if(mode==OUTPUT) LPC_GPIO_PORT->DIR0 |= (1 << pin);
+		else LPC_GPIO_PORT->DIR0 &= ~(1 << pin);
 	}
 }
 void digitalWrite(uint8_t pin, uint8_t value)
@@ -19,7 +37,21 @@ void digitalWrite(uint8_t pin, uint8_t value)
 	{
 		if(value==1) LPC_GPIO_PORT->SET0 = 1 << LED_LOCATION;
 		else LPC_GPIO_PORT->CLR0 = 1 << LED_LOCATION;
+	}else
+	{
+		if(value==1) LPC_GPIO_PORT->SET0 = 1 << pin;
+		else LPC_GPIO_PORT->CLR0 = 1 << pin;
 	}
+}
+
+int digitalRead(uint8_t pin)
+{
+	int result=0;
+	if (LPC_GPIO_PORT->PIN0 & (0x1 << pin))
+	{
+	  result = 1;
+	}
+	return result;
 }
 
 void delay(uint32_t milliSeconds)
--- src/arduino.h
+++ src/arduino.h
@@ -4,10 +4,12 @@
 #include "mrt.h"
 #include "uart.h"
 
-#define OUTPUT 1
-#define LOW 0
-#define HIGH 1
+#define INPUT 	0
+#define OUTPUT 	1
+#define LOW 	0
+#define HIGH 	1
 
 void pinMode(uint8_t pin, uint8_t mode);
 void digitalWrite(uint8_t pin, uint8_t value);
+int digitalRead(uint8_t pin);
 void delay(uint32_t milliSeconds);
