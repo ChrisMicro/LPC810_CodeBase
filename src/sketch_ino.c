25683e24896f812a82e46faf67de4f7de55a4a2b
ChrisMicro <christoph@roboterclub-freiburg.de>
2014-09-12 20:26:20 +0200

    LPC810 pin out added

--- src/sketch_ino.c
+++ src/sketch_ino.c
@@ -1,18 +1,29 @@
 #include "sketch_ino.h"
 
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+     Arduino LPC810 compatibility test program
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
+     LPC800 Pining ( Arduino pin numbering )
+     =======================================
+                      ________
+                      |   U   |
+          reset      -|       |- digital 0 / analog in 0 / RX
+     TX / digital 4  -|  LPC  |- GND
+          digital 3  -|  810  |- +3.3V
+          digital 2  -|       |- digital 1 / analog in 1
+                      |_______|
 
-  This example code is in the public domain.
 
-  modified 8 May 2014
-  by Scott Fitzgerald
+     Mini Kit Board ( http://www.lpcware.com/lpc800-mini-kit )
+     =========================================================
+                      ________
+                      |   U   |
+        reset switch -|       |- RX
+                  TX -|  LPC  |- GND
+                     -|  810  |- +3.3V
+            test led -|       |- ISP switch
+                      |_______|
+
  */
 
 int ledPin=2;
@@ -21,12 +32,19 @@ int ledPin=2;
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(ledPin, OUTPUT);
+  Serial_begin(9600); // attention: slightly different syntax compared to Arduino
+  Serial_println("I'm LPC810");
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
+  delay(100);              		// wait for 00ms
   digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  delay(100);              		// wait for 100ms
+  Serial_print("ms time: ");
+  Serial_printnumber(millis());
+  Serial_print("ADC: ");
+  Serial_printnumber(analogRead(1));
+  Serial_println("");
 }
