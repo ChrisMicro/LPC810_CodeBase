b102ea61d7d36e86154e6f276913d247af89eea9
ChrisMicro <christoph@roboterclub-freiburg.de>
2014-09-12 20:28:44 +0200

    timer functions improved, analogRead added

--- src/arduino.h
+++ src/arduino.h
@@ -3,6 +3,7 @@
 #include "gpio.h"
 #include "mrt.h"
 #include "uart.h"
+#include "adc.h"
 
 #define INPUT 	0
 #define OUTPUT 	1
@@ -13,3 +14,9 @@ void pinMode(uint8_t pin, uint8_t mode);
 void digitalWrite(uint8_t pin, uint8_t value);
 int digitalRead(uint8_t pin);
 void delay(uint32_t milliSeconds);
+void Serial_begin(uint32_t baudRate);
+void Serial_print(char * string);
+void Serial_println(char * string);
+void Serial_printnumber(uint32_t number);
+uint32_t millis();
+uint16_t analogRead(uint8_t channel );
