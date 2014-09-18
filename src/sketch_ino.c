1607ef0aab5075c8b10df920a299ac83d0917e95
chris <christoph@roboterclub-freiburg.de>
2014-09-19 00:48:28 +0200

    analogWrite sin wave dds example added

--- src/sketch_ino.c
+++ src/sketch_ino.c
@@ -1,48 +1,62 @@
-// modified for LPC810 LED on digital 2
 #include "arduino.h"
-
 /*
- Fading
+  simple DDS sin wave generator
 
- This example shows how to fade an LED using the analogWrite() function.
+  DDS:
+    http://en.wikipedia.org/wiki/Direct_digital_synthesizer
 
- The circuit:
- * LED attached from digital pin 9 to ground.
+  generate an audio signal on analog output
 
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
+   created 2014
+   by ChrisMicro
 
- modified and adapted by ChrisMicro 2014 for LPC810
+   This example code is in the public domain.
 
- http://arduino.cc/en/Tutorial/Fading
+*/
 
- This example code is in the public domain.
-
- */
-
-int ledPin = 2;    // LED connected to digital pin 2 on
+int analogOutPin=2;
 
 void setup() {
-  // nothing happens in setup
+  // initialize the digital pin as an output.
+  pinMode(analogOutPin, OUTPUT);
 }
 
-void loop() {
-  // fade in from min to max in increments of 5 points:
-  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
-  }
+int8_t sintab[256]=
+{
+  0 ,3 ,6 ,9 ,12 ,15 ,18 ,21 ,24 ,27 ,30 ,33 ,36 ,39 ,42 ,45 ,48 ,51 ,54 ,57 ,59 ,62 ,65 ,67 ,70 ,73 ,75 ,78 ,80 ,82 ,85 ,87 ,
+  89 ,91 ,94 ,96 ,98 ,100 ,102 ,103 ,105 ,107 ,108 ,110 ,112 ,113 ,114 ,116 ,117 ,118 ,119 ,120 ,121 ,122 ,123 ,123 ,124 ,125 ,
+  125 ,126 ,126 ,126 ,126 ,126 ,127 ,126 ,126 ,126 ,126 ,126 ,125 ,125 ,124 ,123 ,123 ,122 ,121 ,120 ,119 ,118 ,117 ,116 ,114 ,
+  113 ,112 ,110 ,108 ,107 ,105 ,103 ,102 ,100 ,98 ,96 ,94 ,91 ,89 ,87 ,85 ,82 ,80 ,78 ,75 ,73 ,70 ,67 ,65 ,62 ,59 ,57 ,54 ,51 ,
+  48 ,45 ,42 ,39 ,36 ,33 ,30 ,27 ,24 ,21 ,18 ,15 ,12 ,9 ,6 ,3 ,0 ,-3 ,-6 ,-9 ,-12 ,-15 ,-18 ,-21 ,-24 ,-27 ,-30 ,-33 ,-36 ,-39 ,
+  -42 ,-45 ,-48 ,-51 ,-54 ,-57 ,-59 ,-62 ,-65 ,-67 ,-70 ,-73 ,-75 ,-78 ,-80 ,-82 ,-85 ,-87 ,-89 ,-91 ,-94 ,-96 ,-98 ,-100 ,-102 ,
+  -103 ,-105 ,-107 ,-108 ,-110 ,-112 ,-113 ,-114 ,-116 ,-117 ,-118 ,-119 ,-120 ,-121 ,-122 ,-123 ,-123 ,-124 ,-125 ,-125 ,-126 ,
+  -126 ,-126 ,-126 ,-126 ,-127 ,-126 ,-126 ,-126 ,-126 ,-126 ,-125 ,-125 ,-124 ,-123 ,-123 ,-122 ,-121 ,-120 ,-119 ,-118 ,-117 ,
+  -116 ,-114 ,-113 ,-112 ,-110 ,-108 ,-107 ,-105 ,-103 ,-102 ,-100 ,-98 ,-96 ,-94 ,-91 ,-89 ,-87 ,-85 ,-82 ,-80 ,-78 ,-75 ,-73 ,
+  -70 ,-67 ,-65 ,-62 ,-59 ,-57 ,-54 ,-51 ,-48 ,-45 ,-42 ,-39 ,-36 ,-33 ,-30 ,-27 ,-24 ,-21 ,-18 ,-15 ,-12 ,-9 ,-6 ,-3
+};
 
-  // fade out from max to min in increments of 5 points:
-  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+
+int frequency=440;
+int samplingTime_us=100;
+
+int phase=0;
+int phaseDelta;
+
+void loop()
+{
+  phaseDelta=frequency*samplingTime_us*65536/1000000UL;
+
+  while(1)
+  {
+    // sin wave DDS ( direct digital synthesis )
+    int value=128+sintab[(phase>>8)&0xFF];
+
+    analogWrite(analogOutPin,value);
+    // increment phase accumulator
+    phase+=phaseDelta;
+
+    delayMicroseconds(samplingTime_us);
+
   }
 }
 
@@ -61,13 +75,15 @@ void loop() {
 
      Mini Kit Board ( http://www.lpcware.com/lpc800-mini-kit )
      =========================================================
-
-                      _________
+                      ________
                       |   U   |
-        reset switch -|       |-
-                     -|  LPC  |- GND
-         speaker pin -|  810  |- +3.3V
-            test led -|       |-
+        reset switch -|       |- RX
+                  TX -|  LPC  |- GND
+                     -|  810  |- +3.3V
+            test led -|       |- ISP switch
                       |_______|
 
  */
+
+
+
